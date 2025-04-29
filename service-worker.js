72ffbe23e2903548ec71f29c322ac106e6bb352d
Beard <invest@conscious-change.org>
2025-04-28 22:06:35 -0400

    Networking refactored

--- service-worker.js
+++ service-worker.js
@@ -27,6 +27,7 @@ const URLS_TO_CACHE = [
   '/assets/css/pages/industry-research.css',
   '/assets/css/pages/skill-translation.css',
   '/assets/css/pages/resume-personal-branding.css',
+  '/assets/css/pages/networking-strategies.css',
   '/assets/js/script.js',
   '/assets/js/webp-detection.js',
   '/assets/img/hero-bg.jpg',
